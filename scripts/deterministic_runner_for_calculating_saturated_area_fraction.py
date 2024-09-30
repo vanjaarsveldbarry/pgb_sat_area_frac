f400d6c4ee20b2f9dcdb8530bf10c43e4a04d94e
Barry  van Jaarsveld <bvjaarsv@int6.local.snellius.surf.nl>
2024-09-30 20:53:39 +0200

    modify for globgm

--- scripts/deterministic_runner_for_calculating_saturated_area_fraction.py
+++ scripts/deterministic_runner_for_calculating_saturated_area_fraction.py
@@ -18,6 +18,7 @@ import ncConverter as netcdf_writer
 import virtualOS as vos
 
 import logging
+import configparser
 logger = logging.getLogger(__name__)
 
 
@@ -148,7 +149,6 @@ class DeterministicRunner(DynamicModel):
         self.irrTypeFracOverIrr = {}
         self.irrTypeFracOverIrr["irrPaddy"]    = vos.getValDivZero(fraction_paddy_over_irrigated_land, total_irrigated_land_fraction_before_correction)                                                            
         self.irrTypeFracOverIrr["irrNonPaddy"] = vos.getValDivZero(fraction_non_paddy_over_irrigated_land, total_irrigated_land_fraction_before_correction)                                                            
-
         
         # read some land cover parameters, related to Arno scheme
         self.minSoilDepthFrac = {}  
@@ -168,7 +168,6 @@ class DeterministicRunner(DynamicModel):
                                                                    isLddMap         = False, \
                                                                    cover            = None, \
                                                                    isNomMap         = False)
-
 
     def dynamic(self):
 
@@ -199,20 +198,12 @@ class DeterministicRunner(DynamicModel):
             self.fracVegCover = {}
             for coverType in self.coverTypes:
                 if coverType.startswith('irr'):
-		    
-                    self.fractionArea[coverType] = 0.0    # reset (trial)
-                    
-                    # irrigated area for every land cover type (unit: m2)
                     self.fractionArea[coverType] = self.irrTypeFracOverIrr[coverType]* self.irrigationArea # unit: m2
-                    
-                    # fraction of this irrigated area over the entire cell
                     self.fracVegCover[coverType] = pcr.min(1.0, self.fractionArea[coverType] / self.cell_area) 
-		    
                     # avoid small values
                     self.fracVegCover[coverType] = pcr.rounddown(self.fracVegCover[coverType] * 1000.)/1000.
             
-            # rescale land cover fractions (for all land cover types) - this is adopted from the function "scaleModifiedLandCoverFractions()" in the landSurface.py of PCR-GLOBWB
-            # - calculate irrigatedAreaFrac (fraction of irrigation areas) 
+            # rescale land cover fractions (for all land cover types)
             irrigatedAreaFrac = pcr.spatial(pcr.scalar(0.0))
             for coverType in self.coverTypes:
                 if coverType.startswith('irr'):
@@ -222,170 +213,101 @@ class DeterministicRunner(DynamicModel):
             totalArea  = pcr.spatial(pcr.scalar(0.0))
             totalArea += irrigatedAreaFrac
 		    
-            # natural/pristine area fraction (for forest and grassland (pristine Areas))
+            # natural/pristine area fraction (for forest and grassland)
             lcFrac = pcr.max(0.0, 1.0 - totalArea)
-            # - distribute it over every natural land cover type
             pristineAreaFrac = pcr.spatial(pcr.scalar(0.0))
             for coverType in self.coverTypes:         
                 if not coverType.startswith('irr'):
-                    self.fracVegCover[coverType] = 0.0
                     self.fracVegCover[coverType] = self.naturalFracVegCover[coverType] * lcFrac
-                    pristineAreaFrac             = pcr.cover(self.fracVegCover[coverType], 0.0)
+                    pristineAreaFrac += self.fracVegCover[coverType]
 		    
-            # check and make sure that totalArea = 1.0 for all cells
+            # Add pristine area fractions to the total
             totalArea += pristineAreaFrac
-            totalArea = pcr.ifthen(self.landmask,totalArea)
-            totalArea = pcr.cover(totalArea, 1.0)
-            totalArea = pcr.ifthen(self.landmask,totalArea)
-            a,b,c = vos.getMinMaxMean(totalArea - pcr.scalar(1.0))
+
+            # Ensure totalArea is exactly 1.0 across all cells
+            totalArea = pcr.max(0.0, pcr.min(1.0, totalArea))
+
+            # Optionally, rescale land cover fractions so that they sum exactly to 1.0
+            scale_factor = pcr.ifthen(self.landmask, pcr.scalar(1.0) / totalArea)
+            for coverType in self.coverTypes:
+                self.fracVegCover[coverType] = self.fracVegCover[coverType] * scale_factor
+
+            # Check if totalArea equals 1
+            totalArea = pcr.ifthen(self.landmask, totalArea)
+            a, b, c = vos.getMinMaxMean(totalArea - pcr.scalar(1.0))
             threshold = 1e-4
             if abs(a) > threshold or abs(b) > threshold:
-                logger.error("fraction total (from all land cover types) is not equal to 1.0 ... Min %f Max %f Mean %f" %(a,b,c)) 
-
-
-            # read land cover parameters
-            land_cover_fraction = {}
-            minSoilDepthFrac    = {}
-            maxSoilDepthFrac    = {}
-            for coverType in coverTypes:
-                
-                # read land cover fractions
-                land_cover_fraction[coverType] = self.fracVegCover[coverType]
-                
-                # read land cover parameters
-                minSoilDepthFrac[coverType] = self.minSoilDepthFrac[coverType] 
-                maxSoilDepthFrac[coverType] = self.maxSoilDepthFrac[coverType]
-                
-            # calculate tha aggregate land cover parameters
-            minSoilDepthFrac_avg = pcr.scalar(0.0)
-            maxSoilDepthFrac_avg = pcr.scalar(0.0)
-            for coverType in coverTypes:
-                minSoilDepthFrac_avg = minSoilDepthFrac_avg + minSoilDepthFrac[coverType] * land_cover_fraction[coverType]
-                maxSoilDepthFrac_avg = maxSoilDepthFrac_avg + maxSoilDepthFrac[coverType] * land_cover_fraction[coverType]
-                
-            # arnoBeta
-            self.arnoBeta = pcr.max(0.001,\
-                     (maxSoilDepthFrac_avg-1.)/(1.-minSoilDepthFrac_avg)+\
-                                               self.orographyBeta-0.01)
-		    
-            # WMIN (unit: m): minimum local soil water capacity within the grid-cell
-            self.rootZoneWaterStorageMin = minSoilDepthFrac_avg * self.rootZoneWaterStorageCap
-
-
-
+                logger.error("fraction total (from all land cover types) is not equal to 1.0 ... Min %f Max %f Mean %f" % (a,b,c)) 
             
-        # calculating saturated area fraction
+        # Calculating saturated area fraction (the formula can be updated here)
         if self.modelTime.isLastDayOfMonth():
             
             logger.info(" \n\n Calculating for time %s \n\n", self.modelTime.currTime)
 
             # read monthly S1 and S2 (m)
-            monthly_storUpp    = vos.netcdf2PCRobjClone(self.model_setup["monthly_s1_file"], \
-                                                                   "upper_soil_storage", \
-                                                                   str(self.modelTime.fulldate), \
-                                                                   None, \
-                                                                   self.clone)
-            monthly_storLow    = vos.netcdf2PCRobjClone(self.model_setup["monthly_s2_file"], \
-                                                                   "lower_soil_storage", \
-                                                                   str(self.modelTime.fulldate), \
-                                                                   None, \
-                                                                   self.clone)
+            monthly_storUpp = vos.netcdf2PCRobjClone(self.model_setup["monthly_s1_file"], "upper_soil_storage", str(self.modelTime.fulldate), None, self.clone)
+            monthly_storLow = vos.netcdf2PCRobjClone(self.model_setup["monthly_s2_file"], "lower_soil_storage", str(self.modelTime.fulldate), None, self.clone)
             monthly_total_soil_storage = monthly_storUpp + monthly_storLow
             
-
-            # # calculate saturated area fraction
-            # saturated_area_fraction = 1.00 - \
-            #  ((self.rootZoneWaterStorageCap - monthly_total_soil_storage) / (self.rootZoneWaterStorageCap - self.rootZoneWaterStorageMin))**(self.arnoBeta/(self.arnoBeta+1))
-            # # - set the minimum to zero
-            # saturated_area_fraction = pcr.max(0.0, saturated_area_fraction)
-
-            # calculate saturated area fraction
-            # New formula for saturated area fraction
-            # New formula for saturated area fraction using max function
-            saturated_area_fraction = pcr.max((monthly_storUpp + monthly_storLow) / (self.storCapUpp + self.storCapLow))
-
-
-
+            # Calculate saturated area fraction with the provided formula
+            saturated_area_fraction = (monthly_storUpp + monthly_storLow) / (self.storCapUpp + self.storCapLow)
             
-            # reporting 
-            # - time stamp for reporting
-            timeStamp = datetime.datetime(self.modelTime.year,\
-                                          self.modelTime.month,\
-                                          self.modelTime.day,\
-                                          0)
+            # Reporting 
+            timeStamp = datetime.datetime(self.modelTime.year, self.modelTime.month, self.modelTime.day, 0)
             logger.info("Reporting for time %s", self.modelTime.currTime)
-            self.netcdf_report.data2NetCDF(self.model_setup["saturated_area_fraction_output_file"], \
-                                           "satAreaFrac", \
-                                           pcr.pcr2numpy(saturated_area_fraction, vos.MV), \
-                                           timeStamp)
+            self.netcdf_report.data2NetCDF(self.model_setup["saturated_area_fraction_output_file"], "satAreaFrac", pcr.pcr2numpy(saturated_area_fraction, vos.MV), timeStamp)
 
 
 def main():
 
     model_setup = {}
+    # Read the ini file
+    config = configparser.ConfigParser()
+    config.read(sys.argv[1])
 
-    model_setup["clone_file"]     = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/lddsound_05min_version_20210330.map"
-    
-    model_setup["ldd_file"]       = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/lddsound_05min_version_20210330.map"
-    
-    model_setup["cell_area_file"] = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/cdo_gridarea_clone_global_05min_correct_lats.nc"
-
-    model_setup['resVolWC1']      = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/vmcRes_average_1_global_05arcmin.nc"
-    model_setup['resVolWC2']      = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/vmcRes_average_2_global_05arcmin.nc"
-    model_setup['satVolWC1']      = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/vmcSat_average_1_global_05arcmin.nc"
-    model_setup['satVolWC2']      = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/vmcSat_average_2_global_05arcmin.nc"
-
-    model_setup['topo_nc_file']   = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/topography_parameters_5min_april_2021_global_covered_with_zero.nc"
-    
-    model_setup["irrigationArea"] = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/historical_and_ssp_files/irrigated_areas_historical_1960-2019.nc" 
-    
-    # fractions of natural land covers (forest and grassland)
-    model_setup["fraction_forest"]    = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/vegf_tall.map" 
-    model_setup["fraction_grassland"] = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/vegf_short.map" 
-
-
-    # fractions of paddy and non paddy over irrigated areas
-    model_setup["fraction_paddy_over_irrigated_land"]     = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/fractionPaddy_extrapolated.map" 
-    model_setup["fraction_non_paddy_over_irrigated_land"] = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/fractionNonPaddy_extrapolated.map" 
-    
-    model_setup["monthly_s1_file"] = "/projects/0/managed_datasets/hypflowsci6_v1.0/output/gswp3-w5e5/historical-reference/pcrglobwb_cmip6-isimip3-gswp3-w5e5_image-aqueduct_historical-reference_storUppTotal_global_monthly-average_1960_2019_basetier1.nc"
-    model_setup["monthly_s2_file"] = "/projects/0/managed_datasets/hypflowsci6_v1.0/output/gswp3-w5e5/historical-reference/pcrglobwb_cmip6-isimip3-gswp3-w5e5_image-aqueduct_historical-reference_storLowTotal_global_monthly-average_1960_2019_basetier1.nc"
-    
-    model_setup["minSoilDepthFrac_forest"]      = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/minf_tall.map"
-    model_setup["minSoilDepthFrac_grassland"]   = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/minf_short.map"
-    model_setup["minSoilDepthFrac_irrPaddy"]    = "0.99"
-    model_setup["minSoilDepthFrac_irrNonPaddy"] = "0.99"
-    
-    model_setup["maxSoilDepthFrac_forest"]      = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/maxf_tall.map"
-    model_setup["maxSoilDepthFrac_grassland"]   = "/projects/0/dfguu/users/edwin/data/pcrglobwb_input_aqueduct/version_2021-09-16/general/maxf_short.map"
-    model_setup["maxSoilDepthFrac_irrPaddy"]    = "1.01"
-    model_setup["maxSoilDepthFrac_irrNonPaddy"] = "1.01"
-    
-    model_setup["start_date"] = "1960-01-31"
-    model_setup["end_date"]   = "2019-12-31"
-
-    model_setup["output_dir"] = "/scratch-shared/otoo0001/test_sat_area_frac/test/"
-
-    model_setup["saturated_area_fraction_output_file"] = model_setup["output_dir"] + "/" + "estimateSatAreaFrac_monthAvg_" + model_setup["start_date"] + "_to_" + model_setup["end_date"] + ".nc"
-
-
-    print(model_setup["output_dir"])
-    
+    model_setup["output_dir"] = sys.argv[2]
     # make output and temporary folders
     if os.path.exists(model_setup["output_dir"]): shutil.rmtree(model_setup["output_dir"])
     os.makedirs(model_setup["output_dir"])
     # - make temporary folder
     model_setup["tmp_dir"] = model_setup["output_dir"] +  "/tmp/"
     os.makedirs(model_setup["tmp_dir"])
-
-
     # logger
     # - making a log directory
     log_file_directory = model_setup["output_dir"] + "/" + "log/"
     os.makedirs(log_file_directory)
     # - initialize logging
     vos.initialize_logging(log_file_directory)
-    
+
+    model_setup["input_dir"] = config.get('globalOptions', 'inputDir')
+    model_setup["clone_file"] = f"{model_setup['input_dir']}/{config.get('globalOptions', 'cloneMap')}"
+    model_setup["ldd_file"] = f"{model_setup['input_dir']}/{config.get('globalOptions', 'ldd_file')}"
+    model_setup["cell_area_file"] = f"{model_setup['input_dir']}/{config.get('globalOptions', 'cell_area_file')}"
+    model_setup['resVolWC1']      = f"{model_setup['input_dir']}general/vmcRes_average_1_global_05arcmin.nc"
+    model_setup['resVolWC2']      = f"{model_setup['input_dir']}general/vmcRes_average_2_global_05arcmin.nc"
+    model_setup['satVolWC1']      = f"{model_setup['input_dir']}general/vmcSat_average_1_global_05arcmin.nc"
+    model_setup['satVolWC2']      = f"{model_setup['input_dir']}general/vmcSat_average_2_global_05arcmin.nc"
+    model_setup['topo_nc_file']   = f"{model_setup['input_dir']}general/topography_parameters_5min_april_2021_global_covered_with_zero.nc"
+    model_setup["irrigationArea"] = f"{model_setup['input_dir']}historical_and_ssp_files/irrigated_areas_historical_1960-2019.nc" 
+    model_setup["fraction_forest"]    = f"{model_setup['input_dir']}general/vegf_tall.map" 
+    model_setup["fraction_grassland"] = f"{model_setup['input_dir']}general/vegf_short.map" 
+    model_setup["fraction_paddy_over_irrigated_land"]     = f"{model_setup['input_dir']}general/fractionPaddy_extrapolated.map" 
+    model_setup["fraction_non_paddy_over_irrigated_land"] = f"{model_setup['input_dir']}general/fractionNonPaddy_extrapolated.map" 
+    model_setup["minSoilDepthFrac_forest"]      = f"{model_setup['input_dir']}general/minf_tall.map"
+    model_setup["minSoilDepthFrac_grassland"]   = f"{model_setup['input_dir']}general/minf_short.map"
+    model_setup["minSoilDepthFrac_irrPaddy"]    = f"0.99"
+    model_setup["minSoilDepthFrac_irrNonPaddy"] = f"0.99"
+    model_setup["maxSoilDepthFrac_forest"]      = f"{model_setup['input_dir']}general/maxf_tall.map"
+    model_setup["maxSoilDepthFrac_grassland"]   = f"{model_setup['input_dir']}general/maxf_short.map"
+    model_setup["maxSoilDepthFrac_irrPaddy"]    = f"1.01"
+    model_setup["maxSoilDepthFrac_irrNonPaddy"] = f"1.01"
+
+    model_setup["start_date"] = sys.argv[3]
+    model_setup["end_date"]   = sys.argv[4]
+    model_setup["monthly_s1_file"] = sys.argv[5]
+    model_setup["monthly_s2_file"] = sys.argv[6]
+
+    model_setup["saturated_area_fraction_output_file"] = model_setup["output_dir"] + f"sat_area_fraction.nc"
 
     # timeStep info: year, month, day, doy, hour, etc
     currTimeStep = ModelTime() 
@@ -402,4 +324,3 @@ def main():
         
 if __name__ == '__main__':
     sys.exit(main())
-
